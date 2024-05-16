8b865bbe7db3f32df1e6f209fcd88e042ee4af11
Valery Fremaux <valery.fremaux@gmail.com>
2024-05-16 09:01:39 +0200

    Push up 4.1 version

--- version.php
+++ version.php
@@ -27,12 +27,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016082700;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2020060900;        // Requires this Moodle version.
+$plugin->requires  = 2022112801;        // Requires this Moodle version.
 $plugin->component = 'block_shop_course_seats'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.9.0 (Build 2016082700)';
-$plugin->supported = [39, 311];
-$plugin->dependencies = array('local_shop' => 2022090900);
+$plugin->release = '4.1.0 (Build 2016082700)';
+$plugin->supported = [401, 402];
+$plugin->dependencies = array('local_shop' => 2018033000);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.9.0000';
+$plugin->codeincrement = '4.1.0000';
