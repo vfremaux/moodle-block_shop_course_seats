e3df490cba8b00c7a6a9338eb847db64f94513de
Valery Fremaux <valery.fremaux@gmail.com>
2024-10-21 23:50:56 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024011500;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022112801;        // Requires this Moodle version.
 $plugin->component = 'block_shop_discounts'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
